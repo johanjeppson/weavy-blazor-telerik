ba2ca175c2df214a9e0b3f794c6315d281b1489c
Johan Jeppson <johan@mindroute.com>
2021-04-30 11:03:04 +0200

    initial commit

---
+++ weavy-blazor-telerik/weavy-blazor-telerik/Server/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace weavy_blazor_telerik.Server.Models {
+    public class ApplicationUser : IdentityUser {
+    }
+}
